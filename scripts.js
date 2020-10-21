18d6aa1285db2b9a70417690b3de4779563448a5
Aszalea Calderon <aszalea_calderon@yahoo.com>
2020-10-21 08:22:50 -0700

    servers that workgit add scripts.js server.js !

--- scripts.js
+++ scripts.js
@@ -7,22 +7,24 @@ const messageForm = document.getElementById('send-form')
 const messageInput = document.getElementById('message-input')
 
 
-//This takes in a name--
+//This takes in a name--#1
 const name = prompt('What is your awesome name?')
 appendMessage('Welcome! You have signed on!')//This is using the already created function to add a message to the screen
 socket.emit('new-user', name)
 
 
-//It takes the message (messageInput) from above, and adds things here #2
+//It takes the message (messageInput) from above, and adds things here #4
 socket.on('chat-message', (data) => {
   appendMessage(`${data.name}:${data.message}`)//this is sending appendMessage
 })
 
+//#3
 socket.on('user-connected', (name) => {
   appendMessage(`${name} connected`)
   // socket.broadcast.emit = ${name};
 })
 
+//#8
 socket.on('user-disconnected', (name) => {
   appendMessage(`${name} disconnect`)
   // socket.broadcast.emit = ${name};
@@ -32,17 +34,18 @@ socket.on('user-disconnected', (name) => {
 //   appendMessage(`Byee ${name}!`)
 // })
 
+//#6
 messageForm.addEventListener('submit', (e) =>{
   e.preventDefault() //this removes the auto refresh when you send things*/
   const message = messageInput.value //this links to the input type, it saves it*/
-  appendMessage(`you:${message}`)
+  appendMessage(`You:${message}`)
   socket.emit('send-chat-message', message) //every emit takes two paramiters 
   messageInput.value = '' //this emptys out the content from what you just wrote
 }) //e is the standard for event, its just a callback
  //every emit takes two paramiters
 
 
-//Then it goes here #3 
+//Then it goes here #5 
 function appendMessage(message){
   const messageElement = document.createElement('div')//This creates the new message going back
   messageElement.innerText = message//inside the div, we want to add the message
--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const io = require('socket.io')(3000)
 //you need to create an object 
 
 const users={}
-//This is where the path starts - #1
+//This is where the path starts - #2
 io.on('connection', (socket) => {
   socket.on('new-user', name => {
    users[socket.id] = name
     socket.broadcast.emit('user-connected', name)
     // console.log('name-server', name)
   })
+  //#7
   socket.on('send-chat-message', (message) => {
     socket.broadcast.emit('chat-message', {
         message:message,
@@ -18,6 +19,8 @@ io.on('connection', (socket) => {
     })
     
   })
+
+  //#9
   socket.on('disconnect', () => {
     socket.broadcast.emit('user-disconnected', users[socket.id])
     delete users[socket.id]
@@ -30,3 +33,5 @@ io.on('connection', (socket) => {
 // broadcast sends it to everyone else but the sender
 
 //The goal is to get it to print on the screen
+//up arrow enter runs npm start
+//
